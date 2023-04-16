2d746b424fe8829cf76aa49f38d4336b44019cab
ajp <ajphegarty@gmail.com>
2023-04-16 16:30:54 +0100

    .\xml\ws-addr.xsd .\xml\xml.xsd .\xml\xlink.xsd

--- backend/edm/OOTS-EDM/xsd/xsdgen_output.go
+++ backend/edm/OOTS-EDM/xsd/xsdgen_output.go
@@ -4,8 +4,25 @@ package ws
 
 import "encoding/xml"
 
+// May be one of onLoad, onRequest, other, none
+type ActuateType string
+
 // May be one of
 type Anon2 string
+
+type ArcType struct {
+	Title     []string `xml:"http://www.w3.org/1999/xlink title,omitempty"`
+	Type      string   `xml:"type,attr"`
+	Arcrole   string   `xml:"arcrole,attr,omitempty"`
+	TitleAttr string   `xml:"title,attr,omitempty"`
+	Show      string   `xml:"show,attr,omitempty"`
+	Actuate   string   `xml:"actuate,attr,omitempty"`
+	From      string   `xml:"from,attr,omitempty"`
+	To        string   `xml:"to,attr,omitempty"`
+}
+
+// Must be at least 1 items long
+type ArcroleType string
 
 type EndpointReferenceType struct {
 	Items               []string `xml:",any"`
@@ -14,12 +31,35 @@ type EndpointReferenceType struct {
 	Metadata            string   `xml:"http://www.w3.org/2005/08/addressing Metadata,omitempty"`
 }
 
+// Intended for use as the type of user-declared elements to make them
+// extended links.
+// Note that the elements referenced in the content model are all abstract.
+// The intention is that by simply declaring elements with these as their
+// substitutionGroup, all the right things will happen.
+type Extended struct {
+	Title     string `xml:"http://www.w3.org/1999/xlink title,omitempty"`
+	Resource  string `xml:"http://www.w3.org/1999/xlink resource,omitempty"`
+	Locator   string `xml:"http://www.w3.org/1999/xlink locator,omitempty"`
+	Arc       string `xml:"http://www.w3.org/1999/xlink arc,omitempty"`
+	Type      string `xml:"type,attr"`
+	Role      string `xml:"role,attr,omitempty"`
+	TitleAttr string `xml:"title,attr,omitempty"`
+}
+
 type FaultCodesOpenEnumType string
 
 // May be one of tns:InvalidAddressingHeader, tns:InvalidAddress, tns:InvalidEPR, tns:InvalidCardinality, tns:MissingAddressInEPR, tns:DuplicateMessageID, tns:ActionMismatch, tns:MessageAddressingHeaderRequired, tns:DestinationUnreachable, tns:ActionNotSupported, tns:EndpointUnavailable
 type FaultCodesType xml.Name
 
 type Lang string
+
+type LocatorType struct {
+	Title     []string `xml:"http://www.w3.org/1999/xlink title,omitempty"`
+	Type      string   `xml:"type,attr"`
+	Role      string   `xml:"role,attr,omitempty"`
+	TitleAttr string   `xml:"title,attr,omitempty"`
+	Label     string   `xml:"label,attr,omitempty"`
+}
 
 type MetadataType []string
 
@@ -103,5 +143,40 @@ type RelationshipType string
 
 type RelationshipTypeOpenEnum string
 
+type ResourceType struct {
+	Items []string `xml:",any"`
+	Type  string   `xml:"type,attr"`
+	Role  string   `xml:"role,attr,omitempty"`
+	Title string   `xml:"title,attr,omitempty"`
+	Label string   `xml:"label,attr,omitempty"`
+}
+
+// Must be at least 1 items long
+type RoleType string
+
+// May be one of new, replace, embed, other, none
+type ShowType string
+
+// Intended for use as the type of user-declared elements to make them
+// simple links.
+type Simple struct {
+	Items   []string `xml:",any"`
+	Type    string   `xml:"type,attr,omitempty"`
+	Role    string   `xml:"role,attr,omitempty"`
+	Arcrole string   `xml:"arcrole,attr,omitempty"`
+	Title   string   `xml:"title,attr,omitempty"`
+	Show    string   `xml:"show,attr,omitempty"`
+	Actuate string   `xml:"actuate,attr,omitempty"`
+}
+
 // May be one of default, preserve
 type Space string
+
+type TitleEltType struct {
+	Items []string `xml:",any"`
+	Type  string   `xml:"type,attr"`
+	Lang  string   `xml:"lang,attr,omitempty"`
+}
+
+// May be one of simple, extended, title, resource, locator, arc
+type TypeType string
