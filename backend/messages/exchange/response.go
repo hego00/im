63d389bf3a20ff2812cc25f27efb5dd8f4919b79
ajp <ajphegarty@gmail.com>
2023-04-22 18:41:30 +0100

    exchange model files

---
+++ backend/messages/exchange/response.go
@@ -0,0 +1,25 @@
+package exchange
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"aqwari.net/xml/xmltree"
+)
+
+func readQueryExample() {
+	// read xml from file
+	xmlFile, err := ioutil.ReadFile("example.xml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	root, err := xmltree.Parse([]byte(xmlFile))
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, el := range root.Search("", "FullName") {
+		fmt.Printf("%s\n", el.Content)
+	}
+
+}
