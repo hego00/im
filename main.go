ba86b86430faecc91907f5db383677c4a9f799ca
ajp <ajphegarty@gmail.com>
2023-04-22 03:27:37 +0100

    clean demo

--- main.go
+++ main.go
@@ -15,11 +15,6 @@ func main() {
 }
 
 func demo() {
-
-	go backend.Run()
-	// webapp.Run()
-	// wallet.Run()
-
 	liveControl()
 }
 
@@ -33,20 +28,31 @@ func liveControl() {
 
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Welcome!")
-	fmt.Println("Please enter the service you want to start: ")
 
-	service, _ := reader.ReadString('\n')
-	fmt.Printf("Starting %s", service)
+serviceloop:
+	for {
+		fmt.Println("Please enter the service you want to start: ")
 
-	switch {
-	case service == "backend":
-		backend.Run()
-	case service == "webapp":
-		webapp.Run()
-	case service == "wallet":
-		wallet.Run()
-	default:
-		fmt.Println("Service not found")
+		service, _ := reader.ReadString('\n')
+		fmt.Printf("Starting '%s'", service)
+
+		switch {
+		case service == "backend\n":
+			go backend.Run()
+			break
+		case service == "webapp":
+			go webapp.Run()
+			break
+		case service == "wallet":
+			go wallet.Run()
+			break
+		case service == "exit":
+			fmt.Println("Exiting CLI")
+			break serviceloop
+		default:
+			fmt.Println("Service not found")
+		}
+
 	}
 
 }
